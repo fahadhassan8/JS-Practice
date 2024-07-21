e9288fca0c0195146046c6c0b73c4e17b1145c2c
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-07-21 20:38:25 +0500

    updated

---
+++ advance-JS-Class-07/app.js
@@ -0,0 +1,11 @@
+// let age = 20;
+
+// let type = age >= 20 ? "eligible" : "not eligible"
+
+// console.log(type);
+
+// let age = 0;
+// let result = age && "not avail"
+// console.log(result);
+
+
